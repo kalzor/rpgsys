7fe95b0b201a5fc264e5dc560ef682ba7d543167
kalzor <humes.anthony@gmail.com>
2017-04-21 16:16:26 -0500

    Update game.py

--- rpgsys/classes/game.py
+++ rpgsys/classes/game.py
@@ -1,7 +1,7 @@
 import random
 
 
-class bcolors:
+class bColors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -24,6 +24,16 @@ class Person:
         self.magic = magic
         self.actions = ["Attack", "Magic"]
 
-
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
+
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+
+    def take_damage(self, dmg):
+        self.hp -= dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
